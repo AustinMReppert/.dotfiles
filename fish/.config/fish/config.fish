e094022a51951842fd984024a39e939865a261f2
Austin M. Reppert <austinmreppert@protonmail.com>
2020-06-14 20:10:33 -0400

    init

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,11 @@
+set -x PATH $PATH ~/.emacs.d/bin
+
+set -x QT_QPA_PLATFORMTHEME dolphin
+
+alias fix-time "sudo ntpd -qg; sudo hwclock -w"
+
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
