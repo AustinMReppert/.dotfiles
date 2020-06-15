b4f47e402bb463cf8e753b2d93b3867e6e251c45
Austin M. Reppert <austinmreppert@protonmail.com>
2020-06-15 00:17:44 -0400

    remove fish greeting

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,7 +5,10 @@ set -x QT_QPA_PLATFORMTHEME dolphin
 alias fix-time "sudo ntpd -qg; sudo hwclock -w"
 
 if status is-login
-    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        exec startx -- -keeptty
-    end
+  if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+    exec startx -- -keeptty
+  end
+end
+
+function fish_greeting
 end
